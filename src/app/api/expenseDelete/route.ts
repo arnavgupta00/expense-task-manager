69e93d9a83d9f6ca048e737e515f4e83c8c7950d
arnavgupta00 <arnavguptagg@gmail.com>
2024-04-01 03:06:19 +0530

    backend frontend completed, all feature added uses prisma , nextauth ,log in googlr

---
+++ src/app/api/expenseDelete/route.ts
@@ -0,0 +1,51 @@
+import { prismaConnect } from "@/db/prismaGenerate";
+import { NextRequest, NextResponse } from "next/server";
+import bson from "bson";
+
+async function handler(request: Request) {
+  try {
+    const prisma = prismaConnect;
+    const { expenseId, userEmail } = await request.json();
+
+    if (!expenseId || !userEmail) {
+      return NextResponse.json({ error: "Please provide the expense ID and user email" }, { status: 400 });
+    }
+
+    const existingUser = await prisma.user.findUnique({
+      where: {
+        email: userEmail,
+      },
+    });
+
+    if (!existingUser) {
+      return NextResponse.json({ error: "User not found" }, { status: 404 });
+    }
+
+    const existingExpense = await prisma.expense.findUnique({
+      where: {
+        id: parseInt(expenseId),
+      },
+    });
+
+    if (!existingExpense) {
+      return NextResponse.json({ error: "Expense not found" }, { status: 404 });
+    }
+
+    if (existingExpense.userEmail !== userEmail) {
+      return NextResponse.json({ error: "Unauthorized", message: "User is not authorized to delete this expense" }, { status: 403 });
+    }
+
+    await prisma.expense.delete({
+      where: {
+        id: parseInt(expenseId),
+      },
+    });
+
+    return NextResponse.json({ message: "Expense deleted successfully" }, { status: 200 });
+  } catch (error) {
+    console.error("Error deleting expense:", error);
+    return NextResponse.json({ error: "Failed to delete expense", details: error }, { status: 500 });
+  }
+}
+
+export { handler as POST };
